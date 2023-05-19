303f87215477d0ce85d52e5d2de194fbf2f8a236
Bepul-Hossain <bepul.cse.ru@gmail.com>
2023-05-19 18:02:52 +0600

    Updates

---
+++ precache-manifest.8b49180b45613490bd2e7add9fc769a2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d0ba9bab5f8b322fdb33acf29a1404e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b03db8bc873a0ffbbc2",
+    "url": "/static/css/main.f98749f7.chunk.css"
+  },
+  {
+    "revision": "9dd32b90052f2465e6f7",
+    "url": "/static/js/2.20b43afa.chunk.js"
+  },
+  {
+    "revision": "3a5da4106a08e596432e5e2628e1ef8d",
+    "url": "/static/js/2.20b43afa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2b03db8bc873a0ffbbc2",
+    "url": "/static/js/main.7591e571.chunk.js"
+  },
+  {
+    "revision": "c9913191e565de632244",
+    "url": "/static/js/runtime-main.170f8412.js"
+  },
+  {
+    "revision": "c93975e3e5fc5c6bd4d2e789ff31ae4a",
+    "url": "/static/media/cork-wallet.c93975e3.png"
+  },
+  {
+    "revision": "efdba04681a3215e786c0e996e4bc780",
+    "url": "/static/media/desktop13.efdba046.jpg"
+  }
+]);
